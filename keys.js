e828e46ed1e6c4bfe3f5f04d38437beda66061ba
laurengranada <laurengranada@utexas.edu>
2017-01-18 18:03:21 -0600

    adding files

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<CZpBJPkPxgaDwDz4hVb0KA7j7>',
+  consumer_secret: '<owMZPU1nNglZTF2mWHhHRZhtTltpmmULxiD0EZCrBOiI0ZeFOI>',
+  access_token_key: '<37369018-Nqf96LZHc2gkOES2mTxiNlz1V3ZWCEOI73HSxqtLE>',
+  access_token_secret: '<l1FOpRY8qjxKJd9ND8drNJdaXAsq5XdRFDyrSNpO5PDhA>',
+}
